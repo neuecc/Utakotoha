013abf96f3a449dceec5814604785e51147e65aa
neuecc <neuecc@localhost>
2011-03-22 07:19:19 +0900

    work

--- Utakotoha/View/MainPage.xaml.cs
+++ Utakotoha/View/MainPage.xaml.cs
@@ -43,68 +43,73 @@ namespace Utakotoha
 
 
 
-            //Observable.FromEvent<EventArgs>(h => MediaPlayer.ActiveSongChanged += h, h => MediaPlayer.ActiveSongChanged -= h)
-            //    .StartWith(default(IEvent<EventArgs>))
-            //    .Throttle(TimeSpan.FromSeconds(3))
-            //    .ObserveOnDispatcher()
-            //    .Select(_ => (MediaPlayer.State == MediaState.Playing && MediaPlayer.Queue.ActiveSong != null)
-            //            ? new { Artist = MediaPlayer.Queue.ActiveSong.Artist.Name, Song = MediaPlayer.Queue.ActiveSong.Name }
-            //            : null)
-            //    .Where(a => a != null)
-            //    .Select(a => string.Format("site:{0} {1} {2}", GooLyricUri, a.Artist, a.Song))
-            //    .Subscribe(q => LyricBrowser.Navigate(new Uri("http://google.co.jp/search?num=10&q=" + Uri.EscapeUriString(q))));
-
-//            Observable.FromEvent<NavigationEventArgs>(h => LyricBrowser.Navigated += h, h => LyricBrowser.Navigated -= h)
-//                .Where(e => !e.EventArgs.Uri.Host.Contains(GooHost))
-//                .Select(e => (e.Sender as WebBrowser).SaveToString())
-//                .Select(s =>
-//                    (string)LyricBrowser.InvokeScript("eval", @"
-//                        var array = document.getElementsByTagName('a');
-//                        var result = [];
-//                        for(var i = 0; i < array.length; i++)
-//                        {
-//                            var item = array[i];
-//                            if(   item.href.indexOf('index.html') != -1
-//                               && item.innerText.indexOf('歌詞情報 - goo 音楽') != -1)
-//                               //&& item.innerText.indexOf('" + TempArtistName + @"') != -1
-//                               //&& item.innerText.indexOf ('" + TempMusicName + @"') != -1)
-//                            {
-//                                result.push(item);
-//                            }
-//                        };
-//                        
-//                        (result.length != 0) ? result[0].href : ''")
-//                )
-//                .Catch(Observable.Return(""))
-//                .Repeat()
-//                .Subscribe(s =>
-//                {
-//                    if (s.Contains(GooLyricUri)) LyricBrowser.Navigate(new Uri(s));
-//                });
+            Observable.FromEvent<EventArgs>(h => MediaPlayer.ActiveSongChanged += h, h => MediaPlayer.ActiveSongChanged -= h)
+                .Throttle(TimeSpan.FromSeconds(2)) // wait
+                .StartWith(default(IEvent<EventArgs>))
+                .Select(_ => (MediaPlayer.State == MediaState.Playing && MediaPlayer.Queue.ActiveSong != null)
+                        ? new Song { Artist = MediaPlayer.Queue.ActiveSong.Artist.Name, Title = MediaPlayer.Queue.ActiveSong.Name }
+                        : null)
+                .Where(s => s != null)
+                .Select(s => s.SearchLyric())
+                .Switch()
+                .ObserveOnDispatcher();
+            // TODO: show hogehoge.
+                // .Subscribe();
 
 
-//            Observable.FromEvent<NavigationEventArgs>(h => LyricBrowser.Navigated += h, h => LyricBrowser.Navigated -= h)
-//                .Where(e => e.EventArgs.Uri.AbsoluteUri.Contains(GooLyricUri))
-//                .Delay(TimeSpan.FromSeconds(3))
-//                .Take(1)
-//                .Repeat()
-//                .ObserveOnDispatcher()
-//                .Select(e => (e.Sender as WebBrowser).SaveToString())
-//                .Select(s => Regex.Match(s, @"s.setAttribute\('src', '(.+?)'"))
-//                .Where(m => m.Success)
-//                .Select(m => WebRequest.Create(GooUri + m.Groups[1].Value).DownloadStringAsync())
-//                .Switch()
-//                .Select(s => Regex.Replace(s.Trim(), @"^draw\(|\);$", ""))
-//                .Where(s => !string.IsNullOrEmpty(s))
-//                .ObserveOnDispatcher()
-//                .Subscribe(jsonArray =>
-//                {
-//                    LyricBrowser.InvokeScript("eval", @"
-//                        var array = " + jsonArray + @";
-//                        var sb = [];
-//                        for(var i = 0; i < array.length; i++) sb.push(array[i]);
-//                        document.getElementById('lyric_area').innerHTML = sb.join('<br />')");
-//                });
+
+
+            //            Observable.FromEvent<NavigationEventArgs>(h => LyricBrowser.Navigated += h, h => LyricBrowser.Navigated -= h)
+            //                .Where(e => !e.EventArgs.Uri.Host.Contains(GooHost))
+            //                .Select(e => (e.Sender as WebBrowser).SaveToString())
+            //                .Select(s =>
+            //                    (string)LyricBrowser.InvokeScript("eval", @"
+            //                        var array = document.getElementsByTagName('a');
+            //                        var result = [];
+            //                        for(var i = 0; i < array.length; i++)
+            //                        {
+            //                            var item = array[i];
+            //                            if(   item.href.indexOf('index.html') != -1
+            //                               && item.innerText.indexOf('歌詞情報 - goo 音楽') != -1)
+            //                               //&& item.innerText.indexOf('" + TempArtistName + @"') != -1
+            //                               //&& item.innerText.indexOf ('" + TempMusicName + @"') != -1)
+            //                            {
+            //                                result.push(item);
+            //                            }
+            //                        };
+            //                        
+            //                        (result.length != 0) ? result[0].href : ''")
+            //                )
+            //                .Catch(Observable.Return(""))
+            //                .Repeat()
+            //                .Subscribe(s =>
+            //                {
+            //                    if (s.Contains(GooLyricUri)) LyricBrowser.Navigate(new Uri(s));
+            //                });
+
+
+            //            Observable.FromEvent<NavigationEventArgs>(h => LyricBrowser.Navigated += h, h => LyricBrowser.Navigated -= h)
+            //                .Where(e => e.EventArgs.Uri.AbsoluteUri.Contains(GooLyricUri))
+            //                .Delay(TimeSpan.FromSeconds(3))
+            //                .Take(1)
+            //                .Repeat()
+            //                .ObserveOnDispatcher()
+            //                .Select(e => (e.Sender as WebBrowser).SaveToString())
+            //                .Select(s => Regex.Match(s, @"s.setAttribute\('src', '(.+?)'"))
+            //                .Where(m => m.Success)
+            //                .Select(m => WebRequest.Create(GooUri + m.Groups[1].Value).DownloadStringAsync())
+            //                .Switch()
+            //                .Select(s => Regex.Replace(s.Trim(), @"^draw\(|\);$", ""))
+            //                .Where(s => !string.IsNullOrEmpty(s))
+            //                .ObserveOnDispatcher()
+            //                .Subscribe(jsonArray =>
+            //                {
+            //                    LyricBrowser.InvokeScript("eval", @"
+            //                        var array = " + jsonArray + @";
+            //                        var sb = [];
+            //                        for(var i = 0; i < array.length; i++) sb.push(array[i]);
+            //                        document.getElementById('lyric_area').innerHTML = sb.join('<br />')");
+            //                });
         }
     }
 }
