f0844ec4a949b93e31b3ca489bbb5041ab2bbec6
neuecc <neuecc@localhost>
2011-03-31 01:25:54 +0900

    add BingTest

---
+++ Utakotoha.Test/BingTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utakotoha.Model.Bing;
+
+namespace Utakotoha.Model.Test
+{
+    [TestClass]
+    public class BingTest
+    {
+        [TestMethod]
+        public void BuildSearchQuery()
+        {
+            var words = new[]
+            {
+                new SearchWord("homu", SearchMode.All),
+                new SearchWord("mogu", SearchMode.All),
+                new SearchWord("mami", SearchMode.InAnchor),
+                new SearchWord("yellow", SearchMode.InAnchor),
+                new SearchWord("madomado", SearchMode.InBody),
+                new SearchWord("gikagika", SearchMode.InBody),
+                new SearchWord("anko", SearchMode.InTitle),
+                new SearchWord("taiyaki", SearchMode.InTitle),
+            };
+
+            words.BuildQuery().Is("(homu OR mogu) inanchor:(mami OR yellow) inbody:(madomado OR gikagika) intitle:(anko OR taiyaki)");
+        }
+    }
+}
